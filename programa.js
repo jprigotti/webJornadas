66d97b2b2240f89a4c07eb7cab04e908428e08e1
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-10-31 20:30:25 -0300

    added welcome section and agenda

--- programa.js
+++ programa.js
@@ -1,12 +1,32 @@
 const btnDay7 = document.querySelector('#btnDay7');
 btnDay7.addEventListener('click', function () {
     document.querySelector('#btnDay7').classList.add('btnActive');
+    document.querySelector('#btnDay8').classList.remove('btnActive');
     document.querySelector('#btnDay9').classList.remove('btnActive');
     document.querySelector('#btnDay10').classList.remove('btnActive');
     document.querySelector('#day7').classList.add('dayActive');
+    document.querySelector('#day8').classList.remove('dayActive');
     document.querySelector('#day9').classList.remove('dayActive');
     document.querySelector('#day10').classList.remove('dayActive');
     $('.agendaBody .day7').css({ 'display': 'block' });
+    $('.agendaBody .day8').css({ 'display': 'none' });
+    $('.agendaBody .day9').css({ 'display': 'none' });
+    $('.agendaBody .day10').css({ 'display': 'none' });
+
+});
+
+const btnDay8 = document.querySelector('#btnDay8');
+btnDay8.addEventListener('click', function () {
+    document.querySelector('#btnDay7').classList.remove('btnActive');
+    document.querySelector('#btnDay8').classList.add('btnActive');
+    document.querySelector('#btnDay9').classList.remove('btnActive');
+    document.querySelector('#btnDay10').classList.remove('btnActive');
+    document.querySelector('#day7').classList.remove('dayActive');
+    document.querySelector('#day8').classList.add('dayActive');
+    document.querySelector('#day9').classList.remove('dayActive');
+    document.querySelector('#day10').classList.remove('dayActive');
+    $('.agendaBody .day7').css({ 'display': 'none' });
+    $('.agendaBody .day8').css({ 'display': 'block' });
     $('.agendaBody .day9').css({ 'display': 'none' });
     $('.agendaBody .day10').css({ 'display': 'none' });
 
@@ -15,12 +35,15 @@ btnDay7.addEventListener('click', function () {
 const btnDay9 = document.querySelector('#btnDay9');
 btnDay9.addEventListener('click', function () {
     document.querySelector('#btnDay7').classList.remove('btnActive');
+    document.querySelector('#btnDay8').classList.remove('btnActive');
     document.querySelector('#btnDay9').classList.add('btnActive');
     document.querySelector('#btnDay10').classList.remove('btnActive');
     document.querySelector('#day7').classList.remove('dayActive');
+    document.querySelector('#day8').classList.remove('dayActive');
     document.querySelector('#day9').classList.add('dayActive');
     document.querySelector('#day10').classList.remove('dayActive');
     $('.agendaBody .day7').css({ 'display': 'none' });
+    $('.agendaBody .day8').css({ 'display': 'none' });
     $('.agendaBody .day9').css({ 'display': 'block' });
     $('.agendaBody .day10').css({ 'display': 'none' });
 
@@ -29,12 +52,15 @@ btnDay9.addEventListener('click', function () {
 const btnDay10 = document.querySelector('#btnDay10');
 btnDay10.addEventListener('click', function () {
     document.querySelector('#btnDay7').classList.remove('btnActive');
+    document.querySelector('#btnDay8').classList.remove('btnActive');
     document.querySelector('#btnDay9').classList.remove('btnActive');
     document.querySelector('#btnDay10').classList.add('btnActive');
     document.querySelector('#day7').classList.remove('dayActive');
+    document.querySelector('#day8').classList.remove('dayActive');
     document.querySelector('#day9').classList.remove('dayActive');
     document.querySelector('#day10').classList.add('dayActive');
     $('.agendaBody .day7').css({ 'display': 'none' });
+    $('.agendaBody .day8').css({ 'display': 'none' });
     $('.agendaBody .day9').css({ 'display': 'none' });
     $('.agendaBody .day10').css({ 'display': 'block' });
 });
