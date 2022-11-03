1c2657c907af9edc0945f9f8412931723620ce1c
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-11-03 04:05:06 -0300

    new stable version 2_12

--- galeria.js
+++ galeria.js
@@ -5,3 +5,12 @@ $('.imageContainer').hover(function(){
     $('.description').css({'height' : '100%'}) 
     $('.viewGalery').css({'height' : '0%'}) 
 });
+
+$('#obstetriciaGalery').click(function(){
+    $('.popup').css({'display':'block'})
+})
+
+$('.popupContainerClose').click(function (event) {
+    $('.popup').css({ 'display': 'none' });
+    document.body.style.position = '';
+});
