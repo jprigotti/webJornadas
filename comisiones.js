4921a8244a19a8bff8bcada40ca59aa2558ccdcd
titojp <titojp2002@hotmail.com>
2023-10-29 10:20:34 -0300

    starting programa view

--- comisiones.js
+++ comisiones.js
@@ -4,7 +4,7 @@ const dataObjectArray = [
         paragraphs: [
             '<b>Presidenta:</b> Dra. Rodriguez Brieschke Maria Teresa ',
             '<b>Vice Presidenta:</b> Dra. Vivona Maria ',
-            '<b>Secretario General:</b> Dr. Cosso Daniel',
+            '<b>Secretario General:</b> Dr. Coso Daniel',
             '<b>Secretario Adjunto:</b> Dr. Saldarini Fernando',
             '<b>Tesorero:</b> Dr. Iturrieta Pablo',
             '<b>Presidente AMM:</b> Dr. Struminger Marcelo'
@@ -15,7 +15,7 @@ const dataObjectArray = [
         paragraphs: [
             '<b>Presidente:</b> Dr. Bottaro Edgardo',
             '<b>Secretario:</b>  Dr. Katz Marcelo',
-            '<b>Vocales:</b> Dr. Leiguarda Fernando, Lic. Vicenzino Mariana, Rossi Pamela, Dra. Greco Alejandra, Lic. Gregorio Florencia, Guazzaroni Carla, Dra. Geronaso Veronica,  Dr. Ochoa Fabricio, Dr. Gastón Gomez , Daher Jose Barrera Valeria, Dr. Daher Jose, Antonio Pilipek, Dra. Monica Giacco, Dr. Cordoba Pablo, Lini Marina',
+            '<b>Vocales:</b> Dr. Leiguarda Fernando, Lic. Vincenzino Mariana, Rossi Pamela, Dra. Greco Alejandra, Lic. Gregorio Florencia, Guazzaroni Carla, Dra. Geronaso Veronica,  Dr. Ochoa Fabricio, Dr. Gastón Gomez , Daher Jose Barrera Valeria, Dr. Daher Jose, Antonio Pilipek, Dra. Monica Giacco, Dr. Cordoba Pablo, Lini Marina',
         ],
     },
     {
@@ -54,8 +54,8 @@ const dataObjectArray = [
         header: 'Comité de informàtica y redes sociales',
         paragraphs: [
             '<b>Presidente:</b> Dr. Santangelo Pablo ',
-            '<b>Secretario:</b> ',
-            '<b>Vocales:</b> Dra. Del Giudice Gabriela, Dr. Ruben Correa, Dra. Evelina Makara, Marcelo Corigliano, Juan Pablo Rigotti, Julian Nardi, Santiago Miguel Fiorino, Lic. Lerner Melisa, Lic. Dainana Cichello',
+            '<b>Secretario:</b> Juan Pablo Rigotti',
+            '<b>Vocales:</b> Dra. Del Giudice Gabriela, Dr. Ruben Correa, Dra. Evelina Makara, Marcelo Corigliano, Julian Nardi, Santiago Miguel Fiorino, Lic. Lerner Melisa, Lic. Dainana Cichello',
         ],
     },
     {
@@ -95,6 +95,14 @@ const dataObjectArray = [
             '<b>Presidenta:</b> Dra. Silvina Lucilli',
             '<b>Secretario:</b> Dr. Javier Castillo',
             '<b>Vocales:</b> Dr. Bruno Licandro, Dr. Martìn Garcia, Dra. Rocio Alba, Lic. Lourdes Laurens, Lic. Abigail Quiroga, Lic. Lourdes Belay, Dra. Maria Jose Butiler, Dra. Sofia Cribario, Dra. Julieta Carou, Dra. Daniela Araujo, Dra. Magali Vazquez, Dra. Victoria Di Siervi, Dr. Frank Garcia Mendoza, Lic. Moreira Del Corral Macarena, Lic. Arias Rocio, Socióloga Coullery Antonella, Lic. Ponce Sofia'
+        ],
+    },
+    {
+        header: 'Jurado',
+        paragraphs: [
+            '<b>Jurado premio Santojanni:</b> Dr. Domingo Fenoglietto, Dra. Patricia Rey. Dra. Marcela Pelliza',
+            '<b>Jurado premio Salaberry:</b> Dr. Mario Yarrouge, Dr. Jose Cascarino, Dr. Daniel Ballesta',
+            '<b>Jurado premio Dr. Mario Ripoli:</b> Dr. Alberto Valentini, Dra. Patricia Oreiro, Dr. Jorge Bermúdez'
         ],
     },
 
