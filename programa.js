8316b3b9c206dc7db26d8895207048a049d9fa9f
titojp <titojp2002@hotmail.com>
2023-10-31 00:25:16 -0300

    styling program0

--- programa.js
+++ programa.js
@@ -8,18 +8,23 @@ const handleChoseView = (e) => {
     switch (e.target.id) {
         case 'selectDay13':
             $('#viewDay13').removeClass('hide-container');
+            $('.programDay-container').css({'border-left':'8px solid var(--program-color-1)'})
             break;
         case 'selectDay14':
             $('#viewDay14').removeClass('hide-container');
+            $('.programDay-container').css({'border-left':'8px solid var(--program-color-2)'})
             break;
         case 'selectDay15':
             $('#viewDay15').removeClass('hide-container');
+            $('.programDay-container').css({'border-left':'8px solid var(--program-color-3)'})
             break;
         case 'selectDay16':
             $('#viewDay16').removeClass('hide-container');
+            $('.programDay-container').css({'border-left':'8px solid var(--program-color-4)'})
             break;
         case 'selectDay17':
             $('#viewDay17').removeClass('hide-container');
+            $('.programDay-container').css({'border-left':'8px solid var(--program-color-5)'})
             break;
     }
 
