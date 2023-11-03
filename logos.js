83929fb48148556d87535c2aff3e916fb10036d1
jnardi86 <julian.nardi@gmail.com>
2023-11-03 19:50:40 -0300

    Add new section Colaborares

---
+++ logos.js
@@ -0,0 +1,6 @@
+const logosColaboradores = [
+    { name: 'institucional1', src: './img/logos_colaboradores/AMM.svg' },
+    { name: 'institucional2', src: './img/logos_colaboradores/FACU_MEDICINA.png' },
+    { name: 'institucional3', src: './img/logos_colaboradores/IDHS.jpg' },
+   
+];
